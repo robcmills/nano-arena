57fde680be32111d1dea4a789157604db058d04e
Rob Mills <robcmills@gmail.com>
2025-12-08 19:35:40 -0800

    Add click handler for menu bar items

---
+++ input/keyboard.lua
@@ -0,0 +1,18 @@
+local g = require('g')
+
+function love.keypressed(key)
+  if key == 'f' then
+    g.editor.opened_menu = 'file'
+  elseif key == 'e' then
+    g.editor.opened_menu = 'edit'
+  elseif key == 'g' then
+    g.editor.opened_menu = 'grid'
+  elseif key == 'h' then
+    g.editor.opened_menu = 'help'
+  elseif key == 'escape' then
+    g.editor.opened_menu = nil
+  elseif key == 'q' then
+    love.event.quit()
+  end
+end
+
---
+++ input/mouse.lua
@@ -0,0 +1,39 @@
+local g = require('g')
+
+function love.mousemoved(screen_x, screen_y)
+  local x = (screen_x - g.offset_x) / g.scale
+  local y = (screen_y - g.offset_y) / g.scale
+
+  -- menu bar hover
+  for _, key in ipairs(g.editor.menu_bar_order) do
+    local item = g.editor.menu_bar_items[key]
+    item.is_hovered = y < g.editor.menu_bar_height and x >= item.x and x < item.x + item.width
+    if item.is_hovered and g.editor.opened_menu ~= nil then
+      g.editor.opened_menu = key
+    end
+  end
+
+  -- menu hover
+  if g.editor.opened_menu then
+    local menu = g.editor.menus[g.editor.opened_menu]
+    for _, item_id in ipairs(menu.items_order) do
+      local item = menu.items[item_id]
+      item.is_hovered = x >= item.x and x < item.x + item.width and y >= item.y and y < item.y + item.height
+    end
+  end
+end
+
+function love.mousepressed(x, y, button)
+  if button == 1 then
+    -- menu bar
+    for _, key in ipairs(g.editor.menu_bar_order) do
+      local item = g.editor.menu_bar_items[key]
+      if item.is_hovered then
+        g.editor.opened_menu = key
+        return
+      end
+    end
+
+    g.editor.opened_menu = nil
+  end
+end
