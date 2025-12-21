568fca795fd871fdf2510d9fa395cb301a9d1aa9
Rob Mills <robcmills@gmail.com>
2025-12-21 05:38:33 -0800

    Refactor player sprite quads

--- draw/draw_player.lua
+++ draw/draw_player.lua
@@ -1,12 +1,26 @@
 local g = require('g')
 local get_screen_center = require('util/get_screen_center')
 local settings = require('settings')
--- local tile_to_pixel = require('util/tile_to_pixel')
 
 local function draw_player()
   local x, y = get_screen_center()
   local offset = settings.tile_size / 2
-  love.graphics.draw(g.player.sprite_sheet, g.player.sprites.right, x - offset, y - offset)
+
+  local quad = g.player.sprites.right
+  if g.player.dir == 'up' then
+    quad = g.player.sprites.up
+  elseif g.player.dir == 'down' then
+    quad = g.player.sprites.down
+  elseif g.player.dir == 'left' then
+    quad = g.player.sprites.left
+  end
+
+  love.graphics.draw(
+    g.player.sprite_sheet,
+    quad,
+    x - offset,
+    y - offset
+  )
 end
 
 return draw_player
--- load/load_player.lua
+++ load/load_player.lua
@@ -5,16 +5,20 @@ local function load_player()
   g.player.sprite_sheet = love.graphics.newImage("assets/player.png")
   local sheet_width = g.player.sprite_sheet:getWidth()
   local sheet_height = g.player.sprite_sheet:getHeight()
-  local sprite_size = 16
-  g.player.sprite_size = sprite_size
+  local ss = 32
+  g.player.sprite_size = ss
+
   g.player.sprites = {}
-  g.player.sprites.down = love.graphics.newQuad(sprite_size, sprite_size, sprite_size, sprite_size, sheet_width, sheet_height)
-  g.player.sprites.right = love.graphics.newQuad(0, sprite_size, sprite_size, sprite_size, sheet_width, sheet_height)
-  g.player.sprites.up = love.graphics.newQuad(0, 0, sprite_size, sprite_size, sheet_width, sheet_height)
-  g.player.map_pixel_x = 16
-  g.player.map_pixel_y = 16
-  g.player.tile_x = 1
-  g.player.tile_y = 1
+  g.player.sprites.down = love.graphics.newQuad(ss, ss, ss, ss, sheet_width, sheet_height)
+  g.player.sprites.left = love.graphics.newQuad(0, 0, ss, ss, sheet_width, sheet_height)
+  g.player.sprites.right = love.graphics.newQuad(ss, 0, ss, ss, sheet_width, sheet_height)
+  g.player.sprites.up = love.graphics.newQuad(0, ss, ss, ss, sheet_width, sheet_height)
+
+  -- Player starting position
+  g.player.map_pixel_x = 0
+  g.player.map_pixel_y = 0
+  g.player.tile_x = 0
+  g.player.tile_y = 0
   g.player.last_move_time = nil
 end
 
